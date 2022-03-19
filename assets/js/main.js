b78b463dfc0f8deb6c12e214087df8c6df4b938c
Ramón Martínez Cruz <ramcruz1993@gmail.com>
2022-03-19 01:29:19 -0600

    Se agregó transacción a slider clientes.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,36 @@
-// $('.slider').slick({
-//     dots: true,
-//     infinite: true,
-//     speed: 500,
-//     fade: true,
-//     cssEase: 'linear'
-// });
+window.addEventListener("load", function() {
 
-$(document).on("click", ".btnCambirHistori", function(evt){
-    let idHistoriaCliente = evt.target.dataset.idHistoriaCliente;
-    console.log(idHistoriaCliente);
+    // $('.slider').slick({
+    //     dots: true,
+    //     infinite: true,
+    //     speed: 500,
+    //     fade: true,
+    //     cssEase: 'linear'
+    // });
+    
+    let indexHistoriaClienteAnterior = 1;
+    $(document).on("click", ".btnCambirHistori", function(evt){
+        
+        let idHistoriaCliente = evt.target.dataset.idHistoriaCliente;
+        if ( indexHistoriaClienteAnterior != idHistoriaCliente ) {
+            
+            $(".contenedorHistoriasClientes-active").removeClass(["d-flex", "contenedorHistoriasClientes-active"]).addClass("d-none");
+            $(".contenedorHistoriasClientes").removeClass("contenedorHistoriasClientes");
+
+            let contenedorHistoriaCliente = $("[data-historia-cliente="+idHistoriaCliente+"]");
+            contenedorHistoriaCliente.removeClass("d-none").addClass("d-flex");
+            contenedorHistoriaCliente.addClass("contenedorHistoriasClientes");
+            
+            setTimeout(function(){
+                contenedorHistoriaCliente.addClass("contenedorHistoriasClientes-active");
+            }, 100);
+    
+            indexHistoriaClienteAnterior = idHistoriaCliente;
+
+        }
+    
+    });
+
+    console.log(1);
+
 });
